500e048253db0e38111d70e6000d1c845a63b9e6
leonardo-gallegos <renzogallegos96@gmail.com>
2022-08-15 17:27:17 -0500

    Created new react app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import github from './github.png';
+import './App.css';
+
+function App() {
+  return (
+    <div class="mainContainer">
+      <div class="titleContainer">
+        <div class="p1">M</div>
+        <div class="p2">y</div>
+        <div class="p3">&nbsp;</div>
+        <div class="p4">f</div>
+        <div class="p5">i</div>
+        <div class="p6">r</div>
+        <div class="p7">s</div>
+        <div class="p8">t</div>
+        <div class="p9">&nbsp;</div>
+        <div class="p10">R</div>
+        <div class="p11">e</div>
+        <div class="p12">a</div>
+        <div class="p13">c</div>
+        <div class="p14">t</div>
+        <div class="p15">&nbsp;</div>
+        <div class="p16">A</div>
+        <div class="p17">p</div>
+        <div class="p18">p</div>
+      </div>
+      <a href="https://github.com/leonardo-gallegos/CoderHouse-Project-31535"><img src={github} className="App-github" alt="github-logo"/></a>
+    </div>
+  );
+}
+
+export default App;
